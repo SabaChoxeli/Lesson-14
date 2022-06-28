dafee4f35b251d984b1583579fe999980ba2454d
SabaChoxeli <sabachoxeli13@gmail.com>
2022-06-28 13:56:21 +0400

    done javascript

---
+++ js/app.js
@@ -0,0 +1,50 @@
+let firstArr = [5, 10, 15, 20, 25];
+let numAdd = 5 + 10 + 15 + 20 + 25;
+let numAver = numAdd / firstArr.length;
+
+// console.log(numAver);
+
+const persons = [
+  {
+    name: "Saba",
+    age: 20,
+  },
+
+  {
+    name: "Lasha",
+    age: 31,
+  },
+
+  {
+    name: "Mariami",
+    age: 27,
+  },
+
+  {
+    name: "Natia",
+    age: 21,
+  },
+
+  {
+    name: "Giorgi",
+    age: 54,
+  },
+];
+
+let mySelf = {
+  firstName: "Saba",
+  lastName: "Chokheli",
+  address: ["Ketevan Tsamebuli Street", "Avlabari"],
+  age: 20,
+  phoneNumbers: [574747774, 0322747447],
+};
+
+console.log(
+  "My name is " +
+    mySelf.firstName +
+    ", My age is " +
+    mySelf.age +
+    ", My address is " +
+    mySelf.address[0] +
+    "."
+);
